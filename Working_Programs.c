471f668a069f1e9c2ecf16896d82a7ada9c2ace6
Pedro Balby <PedroBalby7@gmail.com>
2023-04-04 09:36:22 -0300

    added sum program

--- Working_Programs.c
+++ Working_Programs.c
@@ -8,13 +8,17 @@ printf("hello world");
     return 0;
 }
 */
-/*
+
 #include <stdio.h>
 
 int main (){
+int num1, num2;
+printf("digite os numeros a serem somados: ");
+scanf("%i %i", &num1,&num2);
+printf("\n%i + %i = %i", num1, num2, num1+num2);
 
 
 
     return 0;
 }
-*/
+
